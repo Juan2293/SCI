a0ee0e8c4eab4cac35b22b71501faba3384153fb
Juan David Riascos <juan12932@hotmail.com>
2018-01-02 13:39:59 -0500

    enviar

--- src/app/components/pedidos/pedidos.component.ts
+++ src/app/components/pedidos/pedidos.component.ts
@@ -14,50 +14,29 @@ export class PedidosComponent implements OnInit {
   constructor(private _pedidosService:PedidosService,
               private _clienteService:ClientesService,
               private activatedRoute:ActivatedRoute) {
-
-
-        this.activatedRoute.params.subscribe(
-          params =>{
-             this._pedidosService.getPedidosByClienteId(params['clienteId']).subscribe(data=>{
-              this.pedidosCliente = data;
-
-              for (let i = 0; i < this.pedidosCliente.length; i++) {
-                  this._pedidosService.getPedidoProductoByPedidoId(this.pedidosCliente[i].pedidoId).subscribe(
-                    data=>{
-                        this.productosPorPedido[i] =data;
-                    }
-                  );
-              }
-
-            });
-            this._clienteService.getClientesById(params['clienteId']).subscribe(cliente=>{
-              this.cliente = cliente;
-            });
-          });
-
-          console.log(this.productosPorPedido);
    }
-getProductosPedido(pedido):any{
-
-  let pedidoProducto:any[]=[];
-  this._pedidosService.getPedidoProductoByPedidoId(pedido.pedidoId).subscribe(data=>{
-    pedidoProducto = data;
-  })
-
-
-return "1";
-
-}
 
 
   ngOnInit() {
+    this.activatedRoute.params.subscribe(
+      params =>{
+         this._pedidosService.getPedidosByClienteId(params['clienteId']).subscribe(data=>{
+          this.pedidosCliente = data;
 
+          for (let i = 0; i < this.pedidosCliente.length; i++) {
+              this._pedidosService.getPedidoProductoByPedidoId(this.pedidosCliente[i].pedidoId).subscribe(
+                data=>{
+                    this.productosPorPedido[i] =data;
+                }
+              );
+          }
+
+        });
+        this._clienteService.getClientesById(params['clienteId']).subscribe(cliente=>{
+          this.cliente = cliente;
+        });
+      });
   }
 
-  // getPedidosByClienteId(clienteId){
-  //   this._pedidosService.getPedidosByClienteId(clienteId).subscribe(pedidos=>{
-  //     this.pedidos=pedidos;
-  //   })
-  // }
 
 }
